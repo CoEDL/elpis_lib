d6a10cc778043699672a0c36e795c293a02d18d6
Harry Keightley <harrykeightley@outlook.com>
2022-10-21 17:09:28 +1000

    Add isolated asr to integration tests

--- tests/transcriber/test_transcribe.py
+++ tests/transcriber/test_transcribe.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from loguru import logger
+from pytest import mark
 
 from elpis.trainer.job import BASE_MODEL
 from elpis.transcriber.transcribe import build_pipeline, transcribe
@@ -9,6 +10,7 @@ DATA_DIR = Path(__file__).parent.parent / "data"
 AUDIO = DATA_DIR / "oily_rag.wav"
 
 
+@mark.integration
 def test_standalone_inference(tmp_path: Path):
     pipeline = build_pipeline(BASE_MODEL, tmp_path)
     preds = pipeline(str(AUDIO), return_timestamps="word")
