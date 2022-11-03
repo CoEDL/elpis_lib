215a8deb6d1d8cf361702e7459a7312bcc5f83ec
Harry Keightley <harrykeightley@outlook.com>
2022-11-03 11:04:06 +1000

    Update exported objects from __init__"s

--- elpis/datasets/__init__.py
+++ elpis/datasets/__init__.py
@@ -1,3 +1,12 @@
 from elpis.datasets.dataset import CleaningOptions, Dataset, ProcessingBatch
+from elpis.datasets.preprocessing import process_batch
+from elpis.datasets.processing import create_dataset, prepare_dataset
 
-__all__ = ["CleaningOptions", "Dataset", "ProcessingBatch"]
+__all__ = [
+    "CleaningOptions",
+    "Dataset",
+    "ProcessingBatch",
+    "process_batch",
+    "create_dataset",
+    "prepare_dataset",
+]
---
+++ elpis/transcriber/__init__.py
@@ -0,0 +1,4 @@
+from elpis.transcriber.results import build_elan, build_text
+from elpis.transcriber.transcribe import build_pipeline, transcribe
+
+__all__ = ["build_elan", "build_text", "build_pipeline", "transcribe"]
