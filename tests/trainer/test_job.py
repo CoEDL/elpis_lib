5c2d51d26e5a039b18c90699f470f2147d841334
Harry Keightley <harrykeightley@outlook.com>
2022-11-04 10:11:57 +1000

    Add tests for failing job serialization and update version

--- tests/trainer/test_job.py
+++ tests/trainer/test_job.py
@@ -1,4 +1,5 @@
 from elpis.trainer import TrainingJob, TrainingOptions, TrainingStatus
+from elpis.trainer.job import BASE_MODEL, SAMPLING_RATE
 
 
 def test_training_options_serialization_round_trip():
@@ -9,3 +10,14 @@ def test_training_options_serialization_round_trip():
 def test_training_job_serialization_round_trip():
     expected = TrainingJob("model", "dataset", TrainingOptions())
     assert expected == TrainingJob.from_dict(expected.to_dict())
+
+
+def test_job_from_basic_dict():
+    data = dict(model_name="a", dataset_name="b", options=TrainingOptions().to_dict())
+    job = TrainingJob.from_dict(data)
+    assert job.model_name == "a"
+    assert job.dataset_name == "b"
+    assert job.options == TrainingOptions()
+    assert job.status == TrainingStatus.WAITING
+    assert job.sampling_rate == SAMPLING_RATE
+    assert job.base_model == BASE_MODEL
