15e9ef9f5d205a43ecb84d761cc5da4858a63940
Harry Keightley <harrykeightley@outlook.com>
2023-07-04 12:04:25 +1000

    Capture tqdm logs by redirecting stderr within context manager

--- tests/trainer/test_utils.py
+++ tests/trainer/test_utils.py
@@ -1,6 +1,8 @@
+import sys
 from pathlib import Path
 
 from loguru import logger
+from transformers.utils import logging
 
 from elpis.trainer.utils import log_to_file
 
@@ -11,6 +13,32 @@ def test_log_to_file_captures_loguru_logs(tmp_path: Path):
     with log_to_file(log_file):
         logger.info("TEST")
 
-    assert log_file.exists()
+    assert log_file.exists(), "Couldn't find log file"
     with open(log_file) as _logs:
         assert "TEST" in _logs.read()
+
+
+def test_log_to_file_captures_hugging_face_logs(tmp_path: Path):
+    log_file = tmp_path / "logs.txt"
+
+    with log_to_file(log_file):
+        logger = logging.get_logger("transformers")
+        logger.info("TEST")
+
+    assert log_file.exists(), "Couldn't find log file"
+    with open(log_file) as _logs:
+        assert "TEST" in _logs.read()
+
+
+def test_log_to_file_captures_hugging_face_tqdm_logs(tmp_path: Path):
+    log_file = tmp_path / "logs.txt"
+
+    original_stderr = sys.stderr
+    with log_to_file(log_file):
+        for _ in logging.tqdm(range(3)):
+            pass
+
+    assert original_stderr is sys.stderr, "Original stderr not replaced!"
+    assert log_file.exists(), "Couldn't find log file"
+    with open(log_file) as _logs:
+        assert len(_logs.read()) > 0, "Didn't capture tqdm logs"
