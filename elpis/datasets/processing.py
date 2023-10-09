70257776394712d0ab9d33942ff64f70a842b634
Harry Keightley <harrykeightley@outlook.com>
2023-10-09 14:36:42 +1000

    Cleanup dataset processing - Force absolute audio path resolution for dataset files - Manually load in audio from paths with librosa - More logging
    .

--- elpis/datasets/processing.py
+++ elpis/datasets/processing.py
@@ -3,6 +3,7 @@ from itertools import chain
 from pathlib import Path
 from typing import Any, Dict, List, Optional
 
+import librosa
 import numpy as np
 import sounddevice as sd
 from datasets import Audio, DatasetDict, load_dataset
@@ -12,10 +13,13 @@ from transformers import Wav2Vec2Processor
 PROCESSOR_COUNT = 4
 AUDIO_COLUMN = "audio"
 SAMPLING_RATE = 16_000
+LOGGING_TRANSCRIPT_SAMPLE = 2
 
 
 def create_dataset(
-    dataset_path: Path, cache_dir: Optional[Path] = None, test_size: float = 0.2
+    dataset_path: Path,
+    cache_dir: Optional[Path] = None,
+    test_size: float = 0.2,
 ) -> DatasetDict:
     """Creates a dataset with test/train splits from the data within a given
     directory.
@@ -33,7 +37,9 @@ def create_dataset(
         for file in os.listdir(dataset_path)
         if (dataset_path / file).suffix == ".json"
     ]
-    logger.debug(f"Transcript file paths sample: {transcript_files[:4]}")
+    logger.debug(
+        f"Transcript file paths sample: {transcript_files[:LOGGING_TRANSCRIPT_SAMPLE]}"
+    )
 
     # Annoying hack
     if cache_dir is not None:
@@ -43,45 +49,44 @@ def create_dataset(
 
     # Convert the audio file name column into the matching audio data
     dataset = dataset.rename_column("audio_file", AUDIO_COLUMN)
-    logger.debug(f"Dataset audio file paths sample: {dataset['train'][AUDIO_COLUMN][:4]}")  # type: ignore
+    logger.debug(f"Dataset audio file paths sample: {dataset['train'][AUDIO_COLUMN][:LOGGING_TRANSCRIPT_SAMPLE]}")  # type: ignore
 
     def resolve_audio_path(row: Dict[str, Any]) -> Dict[str, Any]:
         # Forcefully resolve to same dir as dataset.
         path = dataset_path / Path(row[AUDIO_COLUMN]).name
-        row[AUDIO_COLUMN] = str(path)
+        row[AUDIO_COLUMN] = str(path.absolute())
         return row
 
     dataset = dataset.map(resolve_audio_path)
-    logger.debug(f"Dataset audio file paths post-resolution: {dataset['train'][AUDIO_COLUMN][:4]}")  # type: ignore
-    dataset = dataset.cast_column(AUDIO_COLUMN, Audio(sampling_rate=SAMPLING_RATE))
+    logger.debug(f"Dataset audio file paths post-resolution: {dataset['train'][AUDIO_COLUMN][:LOGGING_TRANSCRIPT_SAMPLE]}")  # type: ignore
 
-    logger.debug(f"Sample audio col values: {dataset['train'][AUDIO_COLUMN][0]}")  # type: ignore
+    def load_audio(batch: Dict) -> Dict:
+        path = batch[AUDIO_COLUMN]
+        data, sr = librosa.load(path, sr=SAMPLING_RATE, mono=True)
 
-    # Play some test audio
-    logger.debug(f"Playing test audio file")
-    data = dataset["train"][AUDIO_COLUMN][0]["array"]  # type: ignore
-    sd.play(data, SAMPLING_RATE, blocking=True)
+        batch["audio"] = {"path": path, "array": data, "sampling_rate": SAMPLING_RATE}
+        return batch
 
+    # dataset = dataset.cast_column(AUDIO_COLUMN, Audio(sampling_rate=SAMPLING_RATE))
+    dataset = dataset.map(load_audio)
+
+    # logger.debug(f"Sample audio col values: {dataset['train'][AUDIO_COLUMN][0]}")  # type: ignore
     return dataset["train"].train_test_split(test_size=test_size)  # type: ignore
 
 
 def prepare_dataset(dataset: DatasetDict, processor: Wav2Vec2Processor) -> DatasetDict:
     """Runs some preprocessing over the given dataset.
 
-    TODO: I'm going to be honest, I have no idea what this does, and need some
-    smart ML knight in shining armour to write a propert description.
-
-    Parameters: dataset: The dataset to apply the preprocessing
+    Parameters:
+        dataset: The dataset on which to apply the preprocessing
         processor: The processor to apply over the dataset
     """
-
-    logger.debug(f"Dataset pre prep: {dataset}")
-    logger.debug(f"Dataset[train] pre prep: {dataset['train']['transcript'][0]}")
+    logger.debug(f"Dataset pre-prep: {dataset}")
+    logger.debug(f"Transcript sample: {dataset['train']['transcript'][0]}")
     logger.debug(
         f'Input array shape:, {np.asarray(dataset["train"][0]["audio"]["array"]).shape}'
     )
     logger.debug(f'Sampling rate:, {dataset["train"][0]["audio"]["sampling_rate"]}')
-    logger.debug(f"Tokenizer vocab: {processor.tokenizer.get_vocab()}")  # type: ignore
 
     def _prepare_dataset(batch: Dict) -> Dict[str, List]:
         # Also from https://huggingface.co/blog/fine-tune-xlsr-wav2vec2
@@ -91,7 +96,6 @@ def prepare_dataset(dataset: DatasetDict, processor: Wav2Vec2Processor) -> Datas
             audio["array"], sampling_rate=audio["sampling_rate"]
         ).input_values[0]
         batch["input_length"] = len(batch["input_values"])
-
         batch["labels"] = processor(text=batch["transcript"]).input_ids
 
         return batch
@@ -100,23 +104,12 @@ def prepare_dataset(dataset: DatasetDict, processor: Wav2Vec2Processor) -> Datas
     # flatten and make unique between datasets
     columns_to_remove = list(set(chain.from_iterable(columns)))
 
-    # Play some test audio
-    logger.debug(f"Playing test audio file before dataset preparation")
-    data = dataset["train"]["audio"][0]["array"]  # type: ignore
-    sd.play(data, SAMPLING_RATE, blocking=True)
-
     dataset = dataset.map(
         _prepare_dataset,
         remove_columns=columns_to_remove,
         num_proc=PROCESSOR_COUNT,
     )
-
     logger.debug(f"Dataset post prep: {dataset}")
     logger.debug(f"Training labels: {dataset['train']['labels'][0]}")
 
-    # Play some test audio
-    logger.debug(f"Playing test audio file after dataset preparation")
-    data = dataset["train"]["input_values"][0]  # type: ignore
-    sd.play(data, SAMPLING_RATE, blocking=True)
-    # logger.debug(f"Training inputs: {dataset['train']['input_values'][0]}")
     return dataset
